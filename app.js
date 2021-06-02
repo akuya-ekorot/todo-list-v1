911c1550202201e50be71e700073292daf5ecdb8
Akuya Ekorot <hello@akuya.co.ke>
2021-06-02 23:45:24 +0300

    pass form data to server

--- app.js
+++ app.js
@@ -2,39 +2,25 @@ const express = require('express');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-    }
 
-    res.render("list", {kindOfDay: day});
-    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", {kindOfDay: day}); 
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.item);
 });
 
 app.listen(process.env.PORT || 3000, () => {
